c6c70741e9bb51ae84387e77942ac88800cdd757
sidrarifali <sidraarifali@gmail.com>
2025-01-14 10:59:16 +0500

    Class 1

---
+++ basic.js
@@ -0,0 +1,42 @@
+// console.log('Hello ITM');
+
+// const helloItm = "Hello ITM 2"; //variable declaration with const, can't redeclare/reassignment
+// const hello2 = 1234;
+
+// let hello = "abc";
+// hello = 1234;
+
+// console.log(hello);
+
+// console.log(typeof hello);
+
+// let testvar;
+// console.log(typeof testvar);
+
+// let valueinNumber = "33";
+// console.log(typeof valueinNumber);
+
+// let checkValue = Boolean()
+// console.log(typeof checkValue);
+
+// console.log(1+2);
+// console.log(1+"2");
+// console.log(1+2-"2");
+
+// let x = 100;
+// let y = ++x // Prefix
+// console.log(y); 
+
+// let x = 100;
+// let y = x++ //Postfix
+// console.log(y);
+
+
+// console.log(2 == '2'); //check value and returns true - js can automatically can type convert to check the variable
+// console.log(2 === '2'); // checks data type and return false
+
+// let url = https:www.sidraarifali%20;
+// url.replace = ('%20','-')
+// console.log(url)
+
+console.log(Math.floor(Math.random() * 10)+ 1) // random no generator between 1 to 10, floor is used to keep the no int
